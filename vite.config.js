14304682b411fa048b23601b0e8e47fac9fa9819
manu-2023 <manumahadev44@gmail.com>
2024-12-30 13:24:17 +0530

    Fix chunking and build optimization

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { hot: true, },
-})
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example of manually splitting large libraries
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit (in KB) if you want to suppress the warning
+  }
+}
+
